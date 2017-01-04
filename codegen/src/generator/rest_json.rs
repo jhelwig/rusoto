afad9336d1a302c680bea5722e4a51ebb7f989a2
Matthew Mayer <matthew.mayer@ionchannel.io>
2017-01-03 22:18:19 -0800

    Codegen uses StatusCode directly again.

--- codegen/src/generator/rest_json.rs
+++ codegen/src/generator/rest_json.rs
@@ -1,5 +1,6 @@
 use inflector::Inflector;
 use regex::{Captures, Regex};
+use reqwest::StatusCode;
 
 use botocore::{Member, Operation, Service, Shape};
 use super::GenerateProtocol;
@@ -115,73 +116,23 @@ impl GenerateProtocol for RestJsonGenerator {
     }
 }
 
+// Used to print the enum value rather than the status code and the canonical reason.
+// For codegen purposes, leaving existing StatusCode Display trait implementation intact.
+// StatusCode::Ok.to_string() prints "200 OK"
+// StatusCode::Ok.enum_as_string() prints "StatusCode::Ok"
+trait CodegenString {
+    fn enum_as_string(&self) -> String;
+}
+impl CodegenString for StatusCode {
+    fn enum_as_string(&self) -> String {
+        format!("StatusCode::{:?}", self)
+    }
+}
+
 fn http_code_to_status_code(code: Option<i32>) -> String {
     match code {
         Some(actual_code) => {
-            // From http://hyper.rs/hyper/v0.9.12/src/hyper/src/status.rs.html and modified to return strings:
-            match actual_code {
-                100 => "StatusCode::Continue".to_string(),
-                101 => "StatusCode::SwitchingProtocols".to_string(),
-                102 => "StatusCode::Processing".to_string(),
-                200 => "StatusCode::Ok".to_string(),
-                201 => "StatusCode::Created".to_string(),
-                202 => "StatusCode::Accepted".to_string(),
-                203 => "StatusCode::NonAuthoritativeInformation".to_string(),
-                204 => "StatusCode::NoContent".to_string(),
-                205 => "StatusCode::ResetContent".to_string(),
-                206 => "StatusCode::PartialContent".to_string(),
-                207 => "StatusCode::MultiStatus".to_string(),
-                208 => "StatusCode::AlreadyReported".to_string(),
-                226 => "StatusCode::ImUsed".to_string(),
-                300 => "StatusCode::MultipleChoices".to_string(),
-                301 => "StatusCode::MovedPermanently".to_string(),
-                302 => "StatusCode::Found".to_string(),
-                303 => "StatusCode::SeeOther".to_string(),
-                304 => "StatusCode::NotModified".to_string(),
-                305 => "StatusCode::UseProxy".to_string(),
-                307 => "StatusCode::TemporaryRedirect".to_string(),
-                308 => "StatusCode::PermanentRedirect".to_string(),
-                400 => "StatusCode::BadRequest".to_string(),
-                401 => "StatusCode::Unauthorized".to_string(),
-                402 => "StatusCode::PaymentRequired".to_string(),
-                403 => "StatusCode::Forbidden".to_string(),
-                404 => "StatusCode::NotFound".to_string(),
-                405 => "StatusCode::MethodNotAllowed".to_string(),
-                406 => "StatusCode::NotAcceptable".to_string(),
-                407 => "StatusCode::ProxyAuthenticationRequired".to_string(),
-                408 => "StatusCode::RequestTimeout".to_string(),
-                409 => "StatusCode::Conflict".to_string(),
-                410 => "StatusCode::Gone".to_string(),
-                411 => "StatusCode::LengthRequired".to_string(),
-                412 => "StatusCode::PreconditionFailed".to_string(),
-                413 => "StatusCode::PayloadTooLarge".to_string(),
-                414 => "StatusCode::UriTooLong".to_string(),
-                415 => "StatusCode::UnsupportedMediaType".to_string(),
-                416 => "StatusCode::RangeNotSatisfiable".to_string(),
-                417 => "StatusCode::ExpectationFailed".to_string(),
-                418 => "StatusCode::ImATeapot".to_string(),
-                421 => "StatusCode::MisdirectedRequest".to_string(),
-                422 => "StatusCode::UnprocessableEntity".to_string(),
-                423 => "StatusCode::Locked".to_string(),
-                424 => "StatusCode::FailedDependency".to_string(),
-                426 => "StatusCode::UpgradeRequired".to_string(),
-                428 => "StatusCode::PreconditionRequired".to_string(),
-                429 => "StatusCode::TooManyRequests".to_string(),
-                431 => "StatusCode::RequestHeaderFieldsTooLarge".to_string(),
-                451 => "StatusCode::UnavailableForLegalReasons".to_string(),
-                500 => "StatusCode::InternalServerError".to_string(),
-                501 => "StatusCode::NotImplemented".to_string(),
-                502 => "StatusCode::BadGateway".to_string(),
-                503 => "StatusCode::ServiceUnavailable".to_string(),
-                504 => "StatusCode::GatewayTimeout".to_string(),
-                505 => "StatusCode::HttpVersionNotSupported".to_string(),
-                506 => "StatusCode::VariantAlsoNegotiates".to_string(),
-                507 => "StatusCode::InsufficientStorage".to_string(),
-                508 => "StatusCode::LoopDetected".to_string(),
-                510 => "StatusCode::NotExtended".to_string(),
-                511 => "StatusCode::NetworkAuthenticationRequired".to_string(),
-                _ => "StatusCode::Ok".to_string(),
-            }
+            StatusCode::from_u16(actual_code as u16).enum_as_string()
         }
         None => "StatusCode::Ok".to_string(),
     }
